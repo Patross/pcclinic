d5b29859c3df45ec027fa8fc2ebd3dc8c78b096b
PatrossDev <contactpatross@gmail.com>
2019-04-02 08:07:46 +0100

    compare parts

--- pcclinic/pcclinic/ComparePartsWindow.xaml.cs
+++ pcclinic/pcclinic/ComparePartsWindow.xaml.cs
@@ -24,6 +24,8 @@ namespace pcclinic
         public ComparePartsWindow()
         {
             InitializeComponent();
+            comboItem1.SelectionChanged += comboItemSelection_Changed;
+            comboItem2.SelectionChanged += comboItemSelection_Changed;
 
             using (var db = new LiteDatabase("pcclinic.db"))
             {
@@ -36,6 +38,41 @@ namespace pcclinic
                 {
                     comboItem1.Items.Add(item.Id + ") " + item.ItemName);
                     comboItem2.Items.Add(item.Id + ") " + item.ItemName);
+                }
+            }
+        }
+
+        private void comboItemSelection_Changed(object sender, SelectionChangedEventArgs e)
+        {
+            if (comboItem1.SelectedIndex != -1 & comboItem2.SelectedIndex != -1)
+            {
+                using (var db = new LiteDatabase("pcclinic.db"))
+                {
+                    LiteCollection<Inventory> inventoryCollection = db.GetCollection<Inventory>("inventory");
+                    IEnumerable<Inventory> itemId = inventoryCollection.Find(x => x.Id == int.Parse(comboItem1.SelectedValue.ToString().ToCharArray()[0].ToString()));
+                    foreach (var item in itemId)
+                    {
+
+                        lblName1.Content = item.ItemName.ToString();
+                        lblType1.Content = item.ItemDescription;
+                        lblQuantity1.Content = item.ItemQuantity;
+                        lblValue1.Content = item.ItemValue;
+                    }
+                    var itemId2 = inventoryCollection.Find(x => x.Id == int.Parse(comboItem2.SelectedValue.ToString().ToCharArray()[0].ToString()));
+                    foreach (var item in itemId2)
+                    {
+
+                        lblName2.Content = item.ItemName;
+                        lblType2.Content = item.ItemDescription;
+                        lblQuantity2.Content = item.ItemQuantity;
+                        lblValue2.Content = item.ItemValue;
+                    }
+
+
+                    //lblName1.Content = comboItem1.SelectedValue.ToString(); lblName2.Content = comboItem2.SelectedValue;
+                    //lblType1.Content = comboItem1.SelectedValue.ToString(); lblType2.Content = comboItem2.SelectedValue;
+                    //lblQuantity1.Content = comboItem1.SelectedValue.ToString(); lblQuantity2.Content = comboItem2.SelectedValue;
+                    //lblValue1.Content = comboItem1.SelectedValue.ToString(); lblValue2.Content = comboItem2.SelectedValue;
                 }
             }
         }
